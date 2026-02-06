7106e9d477a7944b8ee5a59506670947f3b2b271
Sundarampal <sundrampal61@gmail.com>
2026-02-06 12:22:14 +0530

    okjson

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,54 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as https;
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Center(
-          child: Column(
-            children: [
-              Center(
-                child:Center(
-                        child:
-                            Text('Scaffold and Appbar'),
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-
-                      ),
-                    ),
-
-
-
-            ],
-          ),
-        ), centerTitle: true),
-        body: Center(child: Text("I am the body")
-
-        ),
-      ),
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      debugShowMaterialGrid: true,
+      home: Homepage(),
+    );
+  }
+}
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("data"),),
+      body: Center(child: ElevatedButton(onPressed: () async {
+        await Downloader.getDownloadedData(
+          "sundarampal.github.io",
+          "/myjsonfiles/Newsquizess.json",
+        );
+      },
+          child: const Text("Get Data"),
+      ),
+      ),
+    );
 
-
-    ),
-  );
+  }
+}
+class Downloader {
+  static Future getDownloadedData(String baseurl, String parturl) async {
+    try{
+      final url = Uri.https(baseurl,parturl,{});
+      final response = await https.get(url);
+      print(response.statusCode);
+      final jsonResponse = convert.jsonDecode(response.body);
+      print(jsonResponse);
+      return jsonResponse;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
 }
